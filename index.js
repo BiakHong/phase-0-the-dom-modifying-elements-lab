caf8186799d3f8c5ea1131446e10fa9f6e05af56
Lian <biakhong@gmail.com>
2022-04-24 01:09:10 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const element = document.getElementById("main");
+document.body.remove(element);
+
+const newHeader = document.createElement("h1");
+newHeader.id ="victory"
+newHeader.textContent="Lian Hong is the champion"
+document.body.append(newHeader);
